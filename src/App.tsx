155333e9ad23ede08297400c25862b0ce96b4f7f
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-05-02 19:03:23 -0400

    sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
+import Header from './components/sections/header';
+import Hero from './components/sections/hero';
+import About from './components/sections/about';
+import Work from './components/sections/work';
+import Contact from './components/sections/contact';
+import Footer from './components/sections/footer';
+import './styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Hero />
+      <About />
+      <Work />
+      <Contact />
+      <Footer />
     </>
   )
 }
