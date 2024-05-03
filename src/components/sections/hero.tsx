c7edbfbe44f4ffbe8ba46595793ac17884899e4c
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-05-02 20:14:57 -0400

    GSAP

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Hero() { 
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to("#hero h1", { duration: 1, x: 100, ease: "bounce" });
+  }, []);
+
   return (
     <section id="hero">
       <h1 className="text-red-500">Ramona Doby</h1>
