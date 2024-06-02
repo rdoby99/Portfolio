a350d8a3281a68b99995bf32046170943bcbabc4
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-06-02 16:34:18 -0700

    Update

--- src/components/sections/featuredWork.tsx
+++ src/components/sections/featuredWork.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function FeaturedWork() {
   return (
-    <section id="featuredWork" className="min-h-screen">
-      <div className="grid grid-cols-[2fr_3fr_2fr_2fr_3fr_1fr] grid-rows-[repeat(16,_minmax(0,_1fr))] h-full divide-x-2 divide-white bg-waves bg-cover text-white">
+    <section id="featuredWork">
+      <div className="grid grid-cols-[2fr_3fr_2fr_2fr_3fr_1fr] grid-rows-[repeat(16,_minmax(0,_1fr))] min-h-[90vh] divide-x-2 divide-white bg-waves bg-cover text-white">
         <div className="col-span-2 col-start-1 row-span-7 p-8">
           <p className="h6">&#123; Featured &#125;</p>
           <h2 className="h2">Work</h2>
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import spiral from "/spiral.png";
 
 export default function Hero() {
   useEffect(() => {
@@ -36,16 +37,23 @@ export default function Hero() {
       id="hero"
       className="hero text-center flex flex-col gap-2 justify-center items-center min-h-[100vh]"
     >
-      <div className="overflow-hidden relative block">
-        <div className="hero__subtext relative h6">&#123; Welcome &#125;</div>
+      <div className="relative">
+        <div className="overflow-hidden relative block">
+          <div className="hero__subtext relative h6">&#123; Welcome &#125;</div>
+        </div>
+        <h1 className="text-[10rem] relative z-10">
+          Hi THERE
+          <br />
+          <span className="hero__italic italic">NICE</span> to
+          <br />
+          MEET you
+        </h1>
+        <img
+          src={spiral}
+          alt=""
+          className="max-w-sm absolute right-[-14rem] top-1/4"
+        />
       </div>
-      <h1 className="text-[10rem]">
-        Hi THERE
-        <br />
-        <span className="hero__italic italic">NICE</span> to
-        <br />
-        MEET you
-      </h1>
     </section>
   );
 }
