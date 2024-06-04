4bab86298c8fbd78e1fd10e974cc7f31e8d116a9
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-06-04 14:02:10 -0400

    Featured work draft

--- src/components/sections/featuredWork.tsx
+++ src/components/sections/featuredWork.tsx
@@ -3,11 +3,55 @@ import React from "react";
 export default function FeaturedWork() {
   return (
     <section id="featuredWork">
-      <div className="grid grid-cols-[2fr_3fr_2fr_2fr_3fr_1fr] grid-rows-[repeat(16,_minmax(0,_1fr))] min-h-[90vh] divide-x-2 divide-white bg-waves bg-cover text-white">
-        <div className="col-span-2 col-start-1 row-span-7 p-8">
+      <div className="grid grid-cols-[2fr_3fr_2fr_2fr_3fr_1fr] grid-rows-[repeat(16,_minmax(0,_1fr))] min-h-[90vh] bg-waves bg-cover text-white">
+        {/* Col 1 */}
+        <div className="workCard col-start-1 row-start-1 col-span-2 row-span-7 pl-16 flex flex-col justify-center">
           <p className="h6">&#123; Featured &#125;</p>
-          <h2 className="h2">Work</h2>
+          <h2 className="h1 uppercase">Work</h2>
         </div>
+        <div className="workCard row-start-8 col-start-1 row-span-4"></div>
+        <div className="workCard row-start-12 col-start-1 row-span-5 workCard workCard__project">
+          Japanese Reader
+        </div>
+
+        {/* Col 2 */}
+        <div className="workCard row-start-8 col-start-2 row-span-4 workCard__accent"></div>
+        <div className="workCard row-start-12 col-start-2 row-span-5"></div>
+
+        {/* Col 3 */}
+        <div className="workCard row-start-1 col-start-3 row-span-4 workCard__project">
+          Delightly
+        </div>
+        <div className="workCard row-start-5 col-start-3 row-span-3"></div>
+        <div className="workCard row-start-8 col-start-3 row-span-5"></div>
+        <div className="workCard row-start-13 col-start-3 row-span-4 workCard__project">
+          Kao
+        </div>
+
+        {/* Col 4 */}
+        <div className="workCard row-start-1 col-start-4 row-span-4"></div>
+        <div className="workCard row-start-5 col-start-4 row-span-3 workCard__project">
+          SAAFON
+        </div>
+        <div className="workCard row-start-8 col-start-4 row-span-5"></div>
+        <div className="workCard row-start-13 col-start-4 row-span-2 workCard__accent"></div>
+        <div className="workCard row-start-15 col-start-4 row-span-2"></div>
+
+        {/* Col 5 */}
+        <div className="workCard row-start-1 col-start-5 row-span-6"></div>
+        <div className="workCard row-start-7 col-start-5 row-span-1"></div>
+        <div className="workCard row-start-8 col-start-5 row-span-4 workCard__project">
+          John Frieda
+        </div>
+        <div className="workCard row-start-12 col-start-5 row-span-3"></div>
+        <div className="workCard row-start-15 col-start-5 row-span-1 workCard__accent"></div>
+        <div className="workCard row-start-16 col-start-5 row-span-1"></div>
+
+        {/* Col 6 */}
+        <div className="workCard row-start-1 col-start-6 row-span-7 workCard__accent"></div>
+        <div className="workCard row-start-8 col-start-6 row-span-4"></div>
+        <div className="workCard row-start-12 col-start-6 row-span-4"></div>
+        <div className="workCard row-start-16 col-start-6 row-span-1"></div>
       </div>
     </section>
   );
