9004c3ee65494392ea7fd9df1d8311748ba0c7b5
Ramona Doby <74220362+rdoby99@users.noreply.github.com>
2024-06-07 11:46:30 -0400

    pop-up styling

--- src/components/sections/workDialog.tsx
+++ src/components/sections/workDialog.tsx
@@ -7,74 +7,70 @@ export default function WorkDialog({ project, loopIndex }) {
   // console.log(project);
 
   project = project.attributes;
+  const tech_stack = project.tech_stack.split(", ");
 
   return (
     <Dialog>
       <DialogTrigger>
-        <div className="workRow__inner flex md:items-center flex-row gap-4 items-start justify-between">
+        <div className="workRow__inner group flex md:items-center flex-row gap-4 items-start justify-between">
           <div className="flex flex-col md:flex-row justify-between md:min-w-[60%] gap-4 md:gap-12">
             <div className="flex gap-1">
               <div className="p1__medium">0{loopIndex + 1}.</div>
               <h3 className="h2">{project.title}</h3>
             </div>
-            <ul className="ml-8 md:ml-0 flex md:flex-col gap-4">
-              <li>Squarespace</li>
-              <li>CSS</li>
+            <ul className="ml-8 md:ml-0 flex md:flex-col gap-4 items-start">
+              {tech_stack.map((tool, index) => (
+                <li key={index}>{tool}</li>
+              ))}
             </ul>
           </div>
-          <button className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center">
             <span className="hidden md:inline-block uppercase">Learn More</span>
-            <img src={diagonalArrow} alt="" className="md:w-4" />
-          </button>
+            <img
+              src={diagonalArrow}
+              alt=""
+              className="md:w-4 group-hover:rotate-45 transition-all duration-200"
+            />
+          </div>
         </div>
       </DialogTrigger>
       <DialogContent>
-        <h4 className="h5">&#123; The Work &#125;</h4>
         <div className="dialog-content">
           {/* Description */}
-          <div>
-            <h5 className="h2">{project.title}</h5>
-            <p className="p1">{project.description}</p>
-            <button className="flex gap-2 items-center">
-              <span className="hidden md:inline-block uppercase">
-                sparefood.com
-              </span>
-              <img src={diagonalArrow} alt="" className="md:w-4" />
-            </button>
-          </div>
-
-          {/* Image */}
-          <div>
+          <div className="flex flex-col gap-6 pb-8">
+            {/* Image */}
             <img src={sitePic} alt="" />
-          </div>
-
-          {/* Agency and Contributions */}
-          <div>
             <div>
-              <h6>Agency</h6>
-              <p>SMAKK Studios</p>
+              <h5 className="h2 mb-2">{project.title}</h5>
+              <h6 className="text-xs mb-4">Agency: Smakk Studios</h6>
+              <p className="p1 mb-4">{project.description}</p>
             </div>
-            <div>
-              <h6>Contributions</h6>
-              <ul>
-                <li>Front-end Dvelopment</li>
-                <li>Solution Planning</li>
-                <li>Technical Leadership</li>
+            <div className="grid grid-cols-2 md:grid-cols-[1fr_2fr] gap-y-4">
+              <h6>Tech Stack:</h6>
+              <ul className="flex gap-x-4 gap-y-2 p2 flex-wrap">
+                {tech_stack.map((tool, index) => (
+                  <li key={index}>{tool}</li>
+                ))}
+              </ul>
+              {/* Contributions */}
+              <h6 className="mb-4">My&nbsp;Contributions:</h6>
+              <ul className="p2 flex gap-x-4 gap-y-2 flex-wrap">
+                {project.contributions.map((cont, index) => (
+                  <li key={index}>{cont}</li>
+                ))}
               </ul>
             </div>
-          </div>
-
-          {/* Tech Stack */}
-          <div>
-            <h6>Tech Stack</h6>
-            <ul>
-              <li>Shopify</li>
-              <li>Shopify</li>
-              <li>Shopify</li>
-              <li>Shopify</li>
-              <li>Shopify</li>
-              <li>Shopify</li>
-            </ul>
+            <a
+              href={project.url}
+              className="flex gap-2 p1__medium items-center button button--primary group"
+            >
+              <span className="uppercase">Visit Site</span>
+              <img
+                src={diagonalArrow}
+                alt=""
+                className="md:w-4 group-hover:rotate-45 transition-all duration-200"
+              />
+            </a>
           </div>
         </div>
       </DialogContent>
